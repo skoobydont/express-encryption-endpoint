3b0c0ffd7d979d7dd7b563abbfef5ac148b3e2ee
skoobydont <31373232+skoobydont@users.noreply.github.com>
2022-03-07 16:40:45 -0500

    configure & start server. encrypt & decrypt routes available. will need to implement db interactions when available

---
+++ index.js
@@ -0,0 +1,82 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+// CORS & Body Parser
+const cors = require("cors");
+const bodyParser = require("body-parser");
+// Import express
+const express = require("express");
+// Instantiate application
+const app = express();
+// Set port
+const PORT = process.env.PORT || 3000;
+// Handle CORS
+app.use(cors());
+// Handle incoming payload types
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Encryption Helpers
+const crypto = require('crypto');
+const { encrypt, decrypt } = require('./crypto');
+const ivs = {
+  object: process.env.OBJECT_IV,
+  attribute: process.env.ATTRIBUTE_IV,
+  firstName: process.env.FIRST_NAME_IV,
+  lastName: process.env.LAST_NAME_IV,
+};
+// Object Payload SK
+const objSK = process.env.OBJECT_ENCRYPTION_KEY;
+// Generic Attribute SK
+const attrSK = process.env.ATTRIBUTE_ENCRYPTION_KEY;
+// Specific Attribute SKs
+const attrSKs = {
+  firstName: process.env.FIRST_NAME_ENCRYPTION_KEY,
+  lastName: process.env.LAST_NAME_ENCRYPTION_KEY,
+};
+// Encrypt Route
+app.post('/encrypt-user', (req, res) => {
+  console.time('encrypt-user');
+  // handle encrypted payload
+  if (typeof req === 'object') {
+    const objKeys = Object.keys(req.body);
+    let objKIndex = 0;
+    let encryptedObj = {};
+    while (objKIndex < objKeys.length) {
+      encryptedObj = {
+        ...encryptedObj,
+        [`${encrypt(attrSK, ivs.attribute, objKeys[objKIndex])}`]:
+          encrypt(attrSKs[objKeys[objKIndex]], ivs[objKeys[objKIndex]], req.body[objKeys[objKIndex]]),
+      };
+      objKIndex += 1;
+    }
+  }
+  const encryptedObjString = encrypt(objSK, ivs.object, JSON.stringify(encryptedObj));
+  console.timeEnd('encrypt-user');
+  return res.status(200).send({ route: 'Test Route', id: encryptedObjString });
+});
+// Decrypt Route
+app.post('/v1/user', (req, res) => {
+  console.time('decrypt-user');
+  if (req.body.payload) {
+    const decryptedObj = decrypt(objSK, ivs.object, req.body.payload);
+  
+    let result = {};
+    let decryptIndex = 0;
+    while (decryptIndex < (Object.keys(JSON.parse(decryptedObj)).length)) {
+      const decryptedAttr = decrypt(attrSK, ivs.attribute, Object.keys(JSON.parse(decryptedObj))[decryptIndex]);
+  
+      result = {
+        ...result,
+        [`${decryptedAttr}`]: decrypt(
+          attrSKs[decryptedAttr],
+          ivs[decryptedAttr],
+          JSON.parse(decryptedObj)[Object.keys(JSON.parse(decryptedObj))[decryptIndex]]
+        ),
+      }
+      decryptIndex += 1;
+    }
+  }
+  console.timeEnd('decrypt-user');
+  return res.status(200).send('OK');
+});
+
+app.listen(PORT, () => console.log(`port: ${PORT}`));
